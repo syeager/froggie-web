1b077fc985a64caf2372bee2207815a5c67e1855
Steve Yeager <steve.f.yeager@gmail.com>
2022-08-12 14:50:53 -0700

    Copy files from Articlib

---
+++ tools/generateClient.ts
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const commander = require("commander");
+const dotenvFlow = require("dotenv-flow");
+const path = require("path");
+const child_process = require("child_process");
+const prependFile = require("prepend-file");
+const fs = require("fs");
+const clientName = "FroggieClient";
+
+loadEnv();
+const options = loadOptions();
+runNswag(options);
+postProcessClient();
+console.info("Client generation complete");
+
+function loadEnv() {
+  const mode = process.env.NODE_ENV || "development";
+  console.info(`Mode=${mode}`);
+
+  const env = dotenvFlow.config({ node_env: mode, default_node_env: mode });
+  if (env.error) {
+    throw new Error(env.error.message);
+  } else {
+    console.info(env.parsed);
+  }
+}
+
+function loadOptions() {
+  commander.option("-u, --url <url>,", "API URL", process.env.API_URL);
+  commander.option(
+    "-s, --scheme <scheme>,",
+    "HTTP/HTTPS",
+    process.env.API_SCHEME
+  );
+  commander.parse(process.argv);
+
+  const options = commander.opts();
+  console.info(`url=${options.url}`);
+  console.info(`scheme=${options.scheme}`);
+
+  if (!options.url) {
+    throw new Error("url is required");
+  }
+
+  if (!options.scheme) {
+    throw new Error("scheme is required");
+  }
+
+  return options;
+}
+
+function runNswag(options: any) {
+  function onComplete(
+    error: never | null,
+    stdout: string,
+    stderr: string
+  ): void {
+    if (stdout !== "") {
+      console.log(stdout);
+    }
+
+    if (stderr !== "") {
+      console.error(stderr);
+    }
+
+    if (error != null) {
+      throw error;
+    }
+  }
+
+  console.info("Running nswag");
+  const nswagPath = path.join("node_modules", ".bin", "nswag");
+  const variables = `/variables:API_URL=${options.url},SCHEME=${options.scheme}`;
+  const command = `${nswagPath} run ./config/${clientName}.nswag /runtime:Net60 ${variables}`;
+  child_process.execSync(command, onComplete);
+}
+
+function postProcessClient() {
+  const configPath = path.join(
+    __dirname,
+    "..",
+    "config",
+    `${clientName}.nswag`
+  );
+  const nswagConfigString = fs.readFileSync(configPath, "utf8");
+  const nswagConfig = JSON.parse(nswagConfigString);
+  const outputFile =
+    nswagConfig.codeGenerators.openApiToTypeScriptClient.output;
+  const outputPath = path.join(__dirname, "..", "config", outputFile);
+  console.info(`Generated client output file at ${outputPath}`);
+  console.info(
+    "Editing generated client file to be ignored by the ts compiler"
+  );
+  prependFile(outputPath, "/* eslint-disable */\n// @ts-nocheck\n");
+}
---
+++ webpack.config.ts
@@ -0,0 +1,74 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const TsconfigPathsPluginDefault = require("tsconfig-paths-webpack-plugin");
+
+const srcPath = path.resolve(__dirname, "src");
+const distPath = path.resolve(__dirname, "dist");
+
+module.exports = {
+  stats: {
+    errorDetails: true,
+  },
+  entry: [
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:8888",
+    "webpack/hot/only-dev-server",
+    path.resolve(srcPath, "index.tsx"),
+  ],
+  devtool: "source-map",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx", ".json"],
+    plugins: [new TsconfigPathsPluginDefault()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ["file-loader?name=[name].[ext]"], // ?name=[name].[ext] is only necessary to preserve the original file name
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: distPath,
+    publicPath: "/",
+  },
+  context: srcPath,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve("public", "index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "public", "images"), to: distPath },
+        { from: path.resolve(__dirname, "public", "meta"), to: distPath },
+      ],
+    }),
+    // new webpack.HotModuleReplacementPlugin(),
+    //new webpack.NamedModulesPlugin(),
+  ],
+  devServer: {
+    static: distPath,
+    port: 8888,
+    devMiddleware: {
+      publicPath: "/",
+    },
+  },
+};
