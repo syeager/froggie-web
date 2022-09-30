b4a238d630a70d378cc2834a465da35833947fad
Steve <steve.f.yeager@gmail.com>
2022-09-30 14:46:26 -0400

    Update task list every 1s

--- src/_temp/taskList.tsx
+++ src/_temp/taskList.tsx
@@ -4,20 +4,26 @@ import { TaskCard } from "@Tasks";
 import { useEffect, useState } from "react";
 import { Row } from "react-bootstrap";
 
+const updateIntervalMs = 1000;
+
 export function TaskList(): JSX.Element {
   const [tasks, setTasks] = useState(undefined as undefined | Tasks.Task[]);
 
   useEffect(() => {
-    const api = async () => {
-      const client = new FroggieApi.Froggie.Client();
-      const taskPage = await client.getTaskPage_GetPage(undefined, undefined);
-      setTasks(taskPage.obj!.results!.map((dto) => Tasks.createTask(dto)));
-    };
-    api();
+    const interval = setInterval(() => {
+      const api = async () => {
+        const client = new FroggieApi.Froggie.Client();
+        const taskPage = await client.getTaskPage_GetPage(100, undefined);
+        setTasks(taskPage.obj!.results!.map((dto) => Tasks.createTask(dto)));
+      };
+      api();
+    }, updateIntervalMs);
+
+    return () => clearInterval(interval);
   }, []);
 
   const taskCards = tasks
-    ? tasks.map((task, i) => <TaskCard key={i} task={task} />)
+    ? tasks.map((task) => <TaskCard key={task.id} task={task} />)
     : [<div key={-1}>loading</div>];
 
   return (
