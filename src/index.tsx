66b30ce6d9bdf36897dae1eb935d3524a0d7e671
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-15 14:11:34 -0700

    Wrap App in redux Provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./froggie/app";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { store } from "./froggie/app/state/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
