18ea76ee491e5b94a044aa325d8aebf8f3d14d27
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-15 14:13:50 -0700

    Change Group to an interface
    This is needed for redux serialization to work.

--- src/froggie/groups/models/Group.ts
+++ src/froggie/groups/models/Group.ts
@@ -1,16 +1,10 @@
 import { Froggie } from "@Api";
 
-export class Group {
-  public readonly id: string;
-  public readonly name: string;
-
-  constructor(id: string, name: string) {
-    this.id = id;
-    this.name = name;
-  }
+export interface Group {
+  id: string;
+  name: string;
 }
 
-export function createGroup(groupDto: Froggie.GroupDto) {
-  const group = new Group(groupDto.id, groupDto.name);
-  return group;
+export function createGroup(groupDto: Froggie.GroupDto): Group {
+  return groupDto;
 }
