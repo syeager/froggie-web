d952354ee09b6c29e0efd915192e67e29825224d
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-14 19:21:21 -0700

    Import everything from accountStore

--- src/froggie/accounts/index.ts
+++ src/froggie/accounts/index.ts
@@ -1,6 +1,6 @@
 export * from "./commands/logInCommand";
 export * from "./commands/registerCommand";
 export * from "./models/user";
-export { getUser } from "./stores/accountStore";
+export * from "./stores/accountStore";
 export * from "./views/logInPage";
 export * from "./views/registerPage";
