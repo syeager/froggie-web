4e7fb19fa808d43416cdd79986188890fb8ecd48
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-15 16:00:42 -0700

    Return a new object instead of the groupDto
    Was getting an error since the groupDto is not serializable.

--- src/froggie/groups/models/Group.ts
+++ src/froggie/groups/models/Group.ts
@@ -1,10 +1,13 @@
 import { Froggie } from "@Api";
 
 export interface Group {
-  id: string;
-  name: string;
+  readonly id: string;
+  readonly name: string;
 }
 
 export function createGroup(groupDto: Froggie.GroupDto): Group {
-  return groupDto;
+  return {
+    id: groupDto.id,
+    name: groupDto.name,
+  };
 }
