c4b1523bf7cd96220478801347de48827326d76c
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-15 16:02:54 -0700

    Render group name on a task card

--- src/froggie/tasks/components/taskCard.tsx
+++ src/froggie/tasks/components/taskCard.tsx
@@ -1,5 +1,7 @@
+import { RootState } from "@/froggie/app/state/store";
 import { Task } from "@Tasks";
 import { Card } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 type Props = {
   task: Task;
@@ -7,12 +9,14 @@ type Props = {
 
 export function TaskCard(props: Props): JSX.Element {
   const task = props.task;
+  const groups = useSelector((state: RootState) => state.groups.groups);
+  const group = groups.find((g) => g.id == task.groupId);
 
   return (
     <Card className="w-100">
       <Card.Header>{task.title}</Card.Header>
       <Card.Body>
-        <Card.Text>{task.id}</Card.Text>
+        <Card.Text>{group?.name}</Card.Text>
       </Card.Body>
     </Card>
   );
