dbd62062ed1e58f8909aedbac3b6d446ecf92058
Steve <steve.f.yeager@gmail.com>
2024-04-02 18:49:26 -0700

    Move create task button to the right

--- src/froggie/app/views/Footer.tsx
+++ src/froggie/app/views/Footer.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "react-bootstrap";
 
 export function Footer(): JSX.Element {
   return (
-    <Navbar fixed="bottom">
+    <Navbar fixed="bottom" className="justify-content-end">
       <ToggleTaskFormModalButton />
     </Navbar>
   );
