127bd46d8a0fca73892d6e9a521bdb89e9ecf91e
Steve <steve.f.yeager@gmail.com>
2024-04-02 18:49:46 -0700

    Fix nav bar import

--- src/froggie/app/HomePage.tsx
+++ src/froggie/app/HomePage.tsx
@@ -1,6 +1,6 @@
 import { TaskList } from "../tasks/components/taskList";
 import { CreateGroupForm } from "../groups/views/createGroupForm";
-import { NavBar } from "./NavBar";
+import { NavBar } from "./views/NavBar";
 import { Container } from "react-bootstrap";
 import { Footer } from "./views/Footer";
 
